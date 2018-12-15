113629b0df271dd653a9483829ae65607b3cc660
snt23skv <snt23skv@hotmail.com>
2018-12-15 17:22:14 +0400

    added Method Parse in partitial class ContollerParse

--- Calculator/ControllerParse.cs
+++ Calculator/ControllerParse.cs
@@ -8,6 +8,46 @@ namespace Calculator
 {
     public static partial class ControllerParse
     {
+        public static bool Parse(string s,out List<double> n,out List<eOperation> o)
+        {
+            bool res = false;
+            n = new List<double>();
+            o = new List<eOperation>();
 
+            List<string> words = new List<string>(s.Split(' '));
+
+            foreach (string word in words)
+            {
+                
+                if ( double.TryParse(word,out double number))
+                {
+                    n.Add(number);
+                }
+                else
+                {
+                    switch (word)
+                    {
+                        case "+":
+                            o.Add(eOperation.addition);
+                            break;
+                        case "-":
+                            o.Add(eOperation.subtraction);
+                            break;
+                        case "*":
+                            o.Add(eOperation.multiplication);
+                            break;
+                        case "/":
+                            o.Add(eOperation.division);
+                            break;
+                        default:
+                            break;
+                    }                    
+                }
+            }
+
+            res = true;
+
+            return res;
+        }
     }
 }
