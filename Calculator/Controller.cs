7541ff1e84ed1dfc1dbfa9150a898c563c3f39a8
bobryshev_m.v@mail.ru <bobryshev_m.v@mail.ru>
2018-12-15 11:45:29 +0400

    Developed Fish Class Controller

---
+++ Calculator/Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Controller
+    {
+        public static string Addition(int a, int b)
+        {
+
+            return string.Empty;
+        }
+        public static string Substraction(int a, int b)
+        {
+            return string.Empty;
+        }
+        public static string Multiplication(int a, int b)
+        {
+            return string.Empty;
+        }
+        public static string Division(int a, int b)
+        {
+            return string.Empty;
+        }
+
+    }
+}         
+            
+       
+         
+     
+	
+
+	
+
+   
+    
+
+
+
+
+
+
+    
+
