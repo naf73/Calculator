c8da9f1c1445b909c6018004d9ddf20a676d5980
snt23skv <snt23skv@hotmail.com>
2018-12-15 12:14:56 +0400

    fixed bug remove FormLoad

--- Calculator/FormMain.Designer.cs
+++ Calculator/FormMain.Designer.cs
@@ -261,7 +261,6 @@
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculator";
-            this.Load += new System.EventHandler(this.FormMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
